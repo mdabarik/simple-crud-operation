f44e060b52e71f4c06c89ab216d2902a5e30ac5b
mdabarik <mdabarik19@gmail.com>
2023-10-12 04:02:36 +0600

    insert operation added

--- index.js
+++ index.js
@@ -29,9 +29,20 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const userCollection = client.db('userDB').collection('users');
+
+    app.get("/users", async (req, res) => {
+        const cursor = userCollection.find()
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+
     app.post('/users', async (req, res) => {
         const user = req.body;
         console.log(user);
+        const result = await userCollection.insertOne(user)
+        res.send(result)
     })
 
 
