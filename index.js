127a5da0e201d1effe1d6755da0dbf2b64921713
mdabarik <mdabarik19@gmail.com>
2023-10-12 03:13:33 +0600

    auth mongodb added

--- index.js
+++ index.js
@@ -28,12 +28,20 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    app.post('/users', async (req, res) => {
+        const user = req.body;
+        console.log(user);
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
@@ -46,3 +54,4 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
     console.log("Simple crud is running on port: " + port);
 })
+
